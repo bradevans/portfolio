18158b59583c20df54858b578c10c0dd014d92ca
Brad Evans <bevans@meelologic.com>
2016-08-21 12:36:40 -0400

    adding webpack config

---
+++ components/webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: 'main.jsx',
+    output: {
+        // Output the bundled file.
+        path: 'bundle/',
+        // Use the name specified in the entry key as name for the bundle file.
+        filename: 'build.js',
+        publicPath: '/bundle/'
+    },
+    module: {
+        loaders: [
+            {
+                // Test for js or jsx files.
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.sass$/,
+                loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('build.css', {
+            allChunks: true
+        })
+    ],
+    externals: {
+        // Don't bundle the 'react' npm package with the component.
+        'react': 'React'
+    },
+    resolve: {
+        // Include empty string '' to resolve files by their explicit extension
+        // (e.g. require('./somefile.ext')).
+        // Include '.js', '.jsx' to resolve files by these implicit extensions
+        // (e.g. require('underscore')).
+        extensions: ['', '.js', '.jsx', '.sass']
+    },
+    watch: true
+};
