8cb86ba55f8b18ce75a4b388d013a049d25be1e8
Brad Evans <brad@manatechllc.com>
2019-05-29 19:01:30 -0400

    citizenship test

---
+++ citizenship/precache-manifest.9ac80d97632f00c1d704c6a1f61c5eef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "027481a668984dc0446bdc0c0e588809",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e112198a01003013cd6e",
+    "url": "/static/css/main.13fd5f76.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e112198a01003013cd6e",
+    "url": "/static/js/main.6decb8b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
