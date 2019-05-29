c21b531eaa20b158544ea6b24a3cc5ad77dc14f1
Brad Evans <brad@manatechllc.com>
2019-05-29 19:16:00 -0400

    changes relative path

---
+++ citizenship/precache-manifest.bc449f4d532845fcc88d256b2fd1561a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68473597208f8c060bd98d8fbc20d8f0",
+    "url": "/citizenship/index.html"
+  },
+  {
+    "revision": "e112198a01003013cd6e",
+    "url": "/citizenship/static/css/main.13fd5f76.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/citizenship/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e112198a01003013cd6e",
+    "url": "/citizenship/static/js/main.6decb8b1.chunk.js"
+  },
+  {
+    "revision": "8f906f3015c9873bd0da",
+    "url": "/citizenship/static/js/runtime~main.5c545e7e.js"
+  }
+]);
--- citizenship/service-worker.js
+++ citizenship/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ac80d97632f00c1d704c6a1f61c5eef.js"
+  "/citizenship/precache-manifest.bc449f4d532845fcc88d256b2fd1561a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/citizenship/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
